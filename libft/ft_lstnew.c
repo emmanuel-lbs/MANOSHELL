32e7630e3a78736a80ab7a018d3de8ab739ffde5
Romain Zhou <rozhou@z1r7p5.42lyon.fr>
2021-11-12 14:12:49 +0100

    Add lst function in libft and create a list with envp

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rozhou <rozhou@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/27 11:02:05 by rozhou            #+#    #+#             */
+/*   Updated: 2020/11/27 16:03:19 by rozhou           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list *list;
+
+	if (!(list = malloc(sizeof(*list))))
+		return (0);
+	list->content = content;
+	list->next = NULL;
+	return (list);
+}
--- srcs/ft_check_path.c
+++ srcs/ft_check_path.c
@@ -1,14 +1,18 @@
-
 #include "../includes/minishell.h"
 
-void	ft_check_path(t_data *data, char **envp)
+void	ft_check_path(t_struct *s, char **envp)
 {
 	int	i;
 
 	i = 0;
+	while (envp[i])
+	{
+		ft_lstadd_back(&s->env.next, ft_lstnew(envp[i]));
+		i++;
+	}
+	i = 0;
 	while (ft_strncmp(envp[i], "PATH=", 5))
 		i++;
-
-	data->env_path = ft_split((envp[i]), ':');
-	data->env_path[0] = (data->env_path[0] + 5);
+	s->data.env_path = ft_split((envp[i]), ':');
+	s->data.env_path[0] = (s->data.env_path[0] + 5);
 }
