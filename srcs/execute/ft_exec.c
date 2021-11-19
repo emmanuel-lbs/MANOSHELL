c9c72986b19caa09b57c45e5edf69b3672378fa5
Romain Zhou <rozhou@z1r6p5.42lyon.fr>
2021-11-19 11:42:40 +0100

    Multipipe working, need to norm and implement redirection. Still waiting for bob to test further

--- srcs/execute/ft_exec.c
+++ srcs/execute/ft_exec.c
@@ -22,28 +22,87 @@ static int	ft_pathfinder(t_struct *s, int n)
 
 int	ft_exec(t_struct *s)
 {
+	int	fd_in;
 	// ################## On attends bob du parsing ####################################
 
-	t_bob	bob;
+	//TEST 3 PIPES
+	// t_bob	bob;
+	// t_bob	bob2;
+	// t_bob	bob3;
+	// t_bob	bob4;
+	// s->bob = &bob;
+	// s->bob->token = ft_split("ls -l", ' ');
+	// bob2.token = ft_split("wc", ' ');
+	// bob3.token = ft_split("wc", ' ');
+	// bob4.token = ft_split("cat -e", ' ');
+	// bob4.next = NULL;
+	// bob3.next = &bob4;
+	// bob2.next = &bob3;
+	// s->bob->next = &bob2;
 
+	//TEST 2 PIPES
+	t_bob	bob;
+	t_bob	bob2;
+	t_bob	bob3;
 	s->bob = &bob;
 	s->bob->token = ft_split("ls -l", ' ');
+	bob2.token = ft_split("wc", ' ');
+	bob3.token = ft_split("wc", ' ');
+	bob3.next = NULL;
+	bob2.next = &bob3;
+	s->bob->next = &bob2;
+
+	//TEST 1 PIPES
+	// t_bob	bob;
+	// t_bob	bob2;
+	// s->bob = &bob;
+	// s->bob->token = ft_split("ls -l", ' ');
+	// bob2.token = ft_split("wc", ' ');
+	// bob2.next = NULL;
+	// s->bob->next = &bob2;
+
+	//TEST 0 PIPES
+	// t_bob	bob;
+	// s->bob = &bob;
+	// s->bob->token = ft_split("ls -l", ' ');
+	// s->bob->next = NULL;
 
 	// ################## IL EST LA bob du parsing ####################################
-
-	s->data.id1 = fork();
-	if (s->data.id1 == -1)
+	fd_in = 0;
+	// On boucle tant qu'on trouve une commande
+	while (s->bob != NULL)
 	{
-		printf("Fork error\n");
-		return (-1);
+		if (pipe(s->data.end) == -1)
+		{
+			printf("Pipe error\n");
+			return (0);
+		}
+		s->data.id1 = fork();
+		if (s->data.id1 == -1)
+		{
+			printf("Fork error\n");
+			return (-1);
+		}
+		if (s->data.id1 == 0)
+		{	
+			//On change l'input avec l'ancient
+			dup2(fd_in, 0);
+			if (s->bob->next != NULL)
+				dup2(s->data.end[1], 1);
+			close(s->data.end[0]);
+			if (ft_pathfinder(s, -1) == -1)
+				exit(EXIT_SUCCESS);
+			execve(s->bob->token[0], s->bob->token, s->data.envp);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(NULL);
+			close(s->data.end[1]);
+			//on sauvegarde l'input pour le donner au prochain pipe
+			fd_in = s->data.end[0];
+			s->bob = s->bob->next;
+		}
 	}
-	if (s->data.id1 == 0)
-	{
-		if (ft_pathfinder(s, -1) == -1)
-			exit(EXIT_SUCCESS);
-		execve(s->bob->token[0], s->bob->token, s->data.envp);
-		exit(EXIT_SUCCESS);
-	}
-	printf("END\n");
-	return (0);
+	return (0); //37 lignes Fonction erreur pour gagner 6 lignes
 }
