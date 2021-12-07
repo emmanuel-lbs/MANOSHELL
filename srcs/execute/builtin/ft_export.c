08e1a5d1df4955ffc0848f446c13fbdfb460cb52
Blodispic <75845611+Blodispic@users.noreply.github.com>
2021-12-07 09:39:04 +0100

    Update ft_export.c

--- srcs/execute/builtin/ft_export.c
+++ srcs/execute/builtin/ft_export.c
@@ -1,63 +1,59 @@
 #include "../../../includes/minishell.h"
 
-// A TESTER !!!
-// A TESTER !!!
-// A TESTER !!!
-// A TESTER !!!
-// A TESTER !!!
-//void	ft_export(t_struct *s)
-//{
-//	char	*mem;
-//	char	*arg;
-//	int		i;
-//	int		j;
-//
-//	i = 1;
-//	j = 0;
-//	if (s->bob->token[1] == NULL)
-//	{
-//		ft_lst_sort_str(s->env);
-//		while (s->env.next != NULL)
-//		{
-//			s->env = *s->env.next;
-//			mem = ft_strchr(s->env.content, '=');
-//			if (mem)
-//			{
-//				arg = malloc(sizeof(char *) * ft_strlen(mem) + 1);
-//				while(mem && mem[i])
-//				{
-//					arg[j] = mem[i]
-//					i++;
-//					j++;
-//				}
-//				arg[j] = '\0';
-//				i = 1;
-//				j = 0;
-//				printf("declare -x %s\"%s\"",s->env.content - ft_strlen(mem - 1), arg);
-//			}
-//			else
-//				printf("declare -x %s",s->env.content);
-//		}
-//	}
-//	else
-//	{
-//		while (s->bob->token[i])
-//		{
-//			ft_lstadd_front(&s->env.next, ft_lstnew(envp[i]));
-//			i++;
-//		}
-//		
-//	}
-//}
-
-//AVEC UN SPLIT
-t_list	*ft_searchdup(t_struct *s, char *str)
+void	ft_lstprint(t_list lst)
 {
+	while (lst.next != NULL)
+	{
+		lst = lst.next;
+		if (ft_strchr(lst.content, '='))
+		{
+			mem = ft_split(lst.content, '=');
+			printf("declare -x %s=\"", mem[0]);
+			while (mem && mem[i])
+			{
+				printf("%s", mem[i]);
+				i++;
+			}
+			i = 1;
+			printf("\"\n");
+		}
+		else
+			printf("declare -x %s\n",lst.content);
+	}
+}
+
+void	ft_lstsort_str(t_struct *s)
+{
+	t_list	mem;
+	t_list	first;
+	t_list	sortlist;
+	char	*str;
+	
+	first = mem;
+	sortlist = mem;
 	while (s->env.next != NULL)
 	{
 		s->env = *s->env.next;
-		if (s->env.content == 
+		ft_lstadd_back(&mem.next, s->env.content);
 	}
+	mem = first;
+	while (first.next != NULL)
+	{
+		first = first.next;
+		while (mem.next != NULL)
+		{
+			mem = mem.next
+			if (ft_strcmp(first.content, mem.content) > 0)
+			{
+				str = first.content;
+				first.content = mem.content;
+				mem.content = str;
+			}
+		}
+		mem = first;
+	}
+	ft_lstprint(sortlist);
+	//ft_lstclear(sortlist);
 }
 
 void	ft_export(t_struct *s)
@@ -69,28 +65,7 @@ void	ft_export(t_struct *s)
 	i = 1;
 	j = 0;
 	if (s->bob->token[1] == NULL)
-	{
-		//ft_lstsort_str(s->env);
-		while (s->env.next != NULL)
-		{
-			s->env = *s->env.next;
-			if (ft_strchr(s->env.content, '='))
-			{
-				mem = ft_split(s->env.content, '=');
-				printf("declare -x %s=\"", mem[0]);
-				while (mem && mem[i])
-				{
-					printf("%s", mem[i]);
-					i++;
-				}
-				i = 1;
-				printf("\"\n");
-			}
-			else
-				printf("declare -x %s\n",s->env.content);
-		}
-		s->env = *s->first;
-	}
+		ft_lstsort_str(s->env);
 	else
 	{
 		while (s->bob->token[i])
