30977a9ea6c0588db344eb624465865a9e0573d5
Blodispic <75845611+Blodispic@users.noreply.github.com>
2022-01-11 12:19:24 +0100

    ft_export: Add check duplicates and lst_sort modified
    lst_sort is now with pointers, need to test check duplicates and lst_sort Still need to do ft_unset to make export working well

--- srcs/execute/builtin/ft_export.c
+++ srcs/execute/builtin/ft_export.c
@@ -1,5 +1,36 @@
 #include "../../../includes/minishell.h"
 
+
+
+int	ft_strccmp(const char *s1, const char *s2, char c)
+{
+	size_t			i;
+	unsigned char	*ss1;
+	unsigned char	*ss2;
+
+	i = 0;
+	ss1 = (unsigned char *)s1;
+	ss2 = (unsigned char *)s2;
+	printf("%c\n", c);
+	while ((ss1[i] != '\0' || ss2[i] != '\0') && (ss1[i] != c || ss2[i] != c))
+	{
+		if (ss1[i] - ss2[i] != 0)
+			return (ss1[i] - ss2[i]);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_checkdup(t_struct *s, char *str)
+{
+	while (s->env.next != NULL)
+	{
+		s->env = *s->env.next;
+		if (ft_strccmp(s->env.content, str, '=') == 0)
+			ft_unset(s->env);
+	}
+}
+
 void	ft_lstprint(t_list *lst)
 {
 	int		i;
@@ -53,8 +84,8 @@ void	ft_lstsort_str(t_struct *s)
 			if (ft_strcmp(first.content, mem.content) > 0)
 			{
 				str = first.content;
-				first.content = mem.content;
-				mem.content = str;
+				first->content = mem->content;
+				mem->content = &str;
 			}
 		}
 	ft_lstprint(&sortlist);
@@ -115,6 +146,7 @@ void	ft_export(t_struct *s)
 				}
 				if (s->bob->token[i][j] == '=' || !s->bob->token[i][j])
 				{
+					ft_checkdup(s, s->bob->token[i]);
 					ft_lstadd_back(&s->env.next, ft_lstnew(s->bob->token[i]));
 				}
 			}
