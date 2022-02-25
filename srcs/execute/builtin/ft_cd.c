109fe2cf9b78b658bde1c6c671b171c077791c25
Romain Zhou <rozhou@z1r8p3.42lyon.fr>
2022-02-25 14:01:17 +0100

    Some norm

--- srcs/execute/builtin/ft_cd.c
+++ srcs/execute/builtin/ft_cd.c
@@ -6,18 +6,41 @@
 /*   By: rozhou <rozhou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 13:29:40 by rozhou            #+#    #+#             */
-/*   Updated: 2022/02/25 10:44:03 by rozhou           ###   ########.fr       */
+/*   Updated: 2022/02/25 13:33:22 by rozhou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
-static void	ft_cd_home(t_struct *s)
+static void	ft_do_cd(t_struct *s, int *status, char *pwd)
 {
-	char	*pwd;
 	char	*tmp1;
 	char	*tmp2;
 
+	*status = 1;
+	pwd = getcwd(pwd, 0);
+	if (pwd)
+	{
+		tmp1 = s->old_pwd.content;
+		s->old_pwd.content = s->pwd.content;
+		tmp2 = s->pwd.content;
+		s->pwd.content = pwd;
+		s->pwd.content = ft_strjoinfree("PWD=", s->pwd.content, 2);
+		s->old_pwd.content = ft_strjoin("OLD", s->old_pwd.content);
+		if (s->data.n == 1)
+		{
+			free(tmp1);
+			free(tmp2);
+		}
+	}
+	g_errna = 0;
+}
+
+static void	ft_cd_home(t_struct *s, int *status)
+{
+	char	*pwd;
+
+	*status = 1;
 	pwd = NULL;
 	if (chdir(s->home.content + 5))
 	{
@@ -28,32 +51,14 @@ static void	ft_cd_home(t_struct *s)
 		}
 	}
 	else
-	{
-		pwd = getcwd(pwd, 0);
-		if (pwd)
-		{
-			tmp1 = s->old_pwd.content;
-			s->old_pwd.content = s->pwd.content;
-			tmp2 = s->pwd.content;
-			s->pwd.content = pwd;
-			s->pwd.content = ft_strjoinfree("PWD=", s->pwd.content, 2);
-			s->old_pwd.content = ft_strjoin("OLD", s->old_pwd.content);
-			if (s->data.n == 1)
-			{
-				free(tmp1);
-				free(tmp2);
-			}
-		}
-		g_errna = 0;
-	}
+		ft_do_cd(s, status, pwd);
 }
 
-static void	ft_cd_old(t_struct *s)
+static void	ft_cd_old(t_struct *s, int *status)
 {
 	char	*pwd;
-	char	*tmp1;
-	char	*tmp2;
 
+	*status = 1;
 	pwd = NULL;
 	if (s->old_pwd.content == NULL)
 	{
@@ -66,31 +71,12 @@ static void	ft_cd_old(t_struct *s)
 		printf("%s : No such file or directory\n", s->old_pwd.content + 7);
 	}
 	else
-	{
-		pwd = getcwd(pwd, 0);
-		if (pwd)
-		{
-			tmp1 = s->old_pwd.content;
-			s->old_pwd.content = s->pwd.content;
-			tmp2 = s->pwd.content;
-			s->pwd.content = pwd;
-			s->pwd.content = ft_strjoinfree("PWD=", s->pwd.content, 2);
-			s->old_pwd.content = ft_strjoin("OLD", s->old_pwd.content);
-			if (s->data.n == 1)
-			{
-				free(tmp1);
-				free(tmp2);
-			}
-		}
-		g_errna = 0;
-	}
+		ft_do_cd(s, status, pwd);
 }
 
-static void	ft_cd_all(t_struct *s)
+static void	ft_cd_all(t_struct *s, int *status)
 {
 	char	*pwd;
-	char	*tmp1;
-	char	*tmp2;
 
 	pwd = NULL;
 	if (chdir(s->bob->token[1]))
@@ -99,54 +85,45 @@ static void	ft_cd_all(t_struct *s)
 		printf("%s : No such file or directory\n", s->bob->token[1]);
 	}
 	else
+		ft_do_cd(s, status, pwd);
+}
+
+static void	ft_putenv(t_struct *s)
+{
+	if (ft_strnncmp("PWD=", s->env->content, 3) == 0)
 	{
-		pwd = getcwd(pwd, 0);
-		if (pwd)
-		{
-			tmp1 = s->old_pwd.content;
-			s->old_pwd.content = s->pwd.content;
-			tmp2 = s->pwd.content;
-			s->pwd.content = pwd;
-			s->pwd.content = ft_strjoinfree("PWD=", s->pwd.content, 2);
-			s->old_pwd.content = ft_strjoin("OLD", s->old_pwd.content);
-			if (s->data.n == 1)
-			{
-				free(tmp1);
-				free(tmp2);
-			}
-		}
-		g_errna = 0;
+		if (s->data.n == 1)
+			free(s->env->content);
+		s->env->content = ft_strdup(s->pwd.content);
+	}
+	else if (ft_strnncmp("OLDPWD=", s->env->content, 6) == 0)
+	{
+		if (s->data.n == 1)
+			free(s->env->content);
+		s->env->content = ft_strdup(s->old_pwd.content);
 	}
 }
 
 void	ft_cd(t_struct *s)
 {
-	char	*pwd;
 	int		i;
+	int		status;
 
-	pwd = NULL;
+	status = 0;
 	i = 0;
 	if (!s->bob->token[1])
-		ft_cd_home(s);
+		ft_cd_home(s, &status);
 	else if (ft_strcmp(s->bob->token[1], "-") == 0)
-		ft_cd_old(s);
+		ft_cd_old(s, &status);
 	else
-		ft_cd_all(s);
-	while (s->env != NULL)
+		ft_cd_all(s, &status);
+	if (status == 1)
 	{
-		if (ft_strnncmp("PWD=", s->env->content, 3) == 0)
+		while (s->env != NULL)
 		{
-			if (s->data.n == 1)
-				free(s->env->content);
-			s->env->content = ft_strdup(s->pwd.content);
+			ft_putenv(s);
+			s->env = s->env->next;
 		}
-		else if (ft_strnncmp("OLDPWD=", s->env->content, 6) == 0)
-		{
-			if (s->data.n == 1)
-				free(s->env->content);
-			s->env->content = ft_strdup(s->old_pwd.content);
-		}
-		s->env = s->env->next;
 	}
 	s->env = s->first;
 	s->data.n = 1;
