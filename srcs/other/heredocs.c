d29554b0cc3415e1fae0a2f13d5d89d3ec476780
emmanuel-lbs <59801700+emmanuel-lbs@users.noreply.github.com>
2022-03-02 12:49:55 +0100

    normanu

--- srcs/other/heredocs.c
+++ srcs/other/heredocs.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredocs.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elabasqu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 12:45:20 by elabasqu          #+#    #+#             */
+/*   Updated: 2022/03/02 12:48:25 by elabasqu         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
@@ -17,57 +28,6 @@ t_bob	*heredocs_bob(t_bob *bob)
 	printf("error\n");
 	exit(-1);
 	return (bob);
-}
-
-char	*modif_here_dol(char *str, char *dollars, t_struct *s)
-{
-	int		i;
-	int		j;
-	int		k;
-	char	*newstr;
-
-	j = 0;
-	i = 0;
-	newstr = malloc(sizeof(char) * (ft_strlen(str) + ft_strlen(dollars)));
-	while (str[i])
-	{
-		if (str[i] == '$')
-		{
-			k = 0;
-			while (dollars[k])
-				add_char(newstr, dollars, &j, &k);
-			free(one_token(str, &i, s));
-		}
-		else
-			add_char(newstr, str, &j, &i);
-	}
-	newstr[j] = 0;
-	return (newstr);
-}
-
-char	*dollars_hered(char *str, t_struct *s)
-{
-	int		i;
-	char	*dollars;
-	char	*new_str;
-
-	i = 0;
-	new_str = NULL;
-	while (str[i])
-	{
-		if (str[i] == '$')
-		{
-			if (new_str != NULL)
-				free(new_str);
-			dollars = one_token(str, &i, s);
-			new_str = modif_here_dol(str, dollars, s);
-			free(dollars);
-		}
-		else
-			i++;
-	}
-	free(str);
-	return (new_str);
 }
 
 char	*heredocs(char	*end_word, t_struct *s)
--- srcs/other/heredocs_utils.c
+++ srcs/other/heredocs_utils.c
@@ -6,7 +6,7 @@
 /*   By: elabasqu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 14:11:14 by elabasqu          #+#    #+#             */
-/*   Updated: 2022/02/28 13:34:01 by elabasqu         ###   ########lyon.fr   */
+/*   Updated: 2022/03/02 12:47:57 by elabasqu         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,55 @@ char	*heredocs_end_word(char	**token, int i)
 	if (token[i] && strcmp("<<", token[i]) != 0)
 		return (NULL);
 	return (token[i + 1]);
+}
+
+char	*modif_here_dol(char *str, char *dollars, t_struct *s)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	*newstr;
+
+	j = 0;
+	i = 0;
+	newstr = malloc(sizeof(char) * (ft_strlen(str) + ft_strlen(dollars)));
+	while (str[i])
+	{
+		if (str[i] == '$')
+		{
+			k = 0;
+			while (dollars[k])
+				add_char(newstr, dollars, &j, &k);
+			free(one_token(str, &i, s));
+		}
+		else
+			add_char(newstr, str, &j, &i);
+	}
+	newstr[j] = 0;
+	return (newstr);
+}
+
+char	*dollars_hered(char *str, t_struct *s)
+{
+	int		i;
+	char	*dollars;
+	char	*new_str;
+
+	i = 0;
+	new_str = NULL;
+	while (str[i])
+	{
+		if (str[i] == '$')
+		{
+			if (new_str != NULL)
+				free(new_str);
+			dollars = one_token(str, &i, s);
+			new_str = modif_here_dol(str, dollars, s);
+			free(dollars);
+		}
+		else
+			i++;
+	}
+	free(str);
+	return (new_str);
 }
