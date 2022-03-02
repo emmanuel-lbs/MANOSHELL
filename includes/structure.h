a9802eb43b0177ab012f5275eeaaeba1ab063b0a
Romain Zhou <rozhou@z1r6p3.42lyon.fr>
2022-03-02 12:40:29 +0100

    No more leaks in atexit

--- includes/structure.h
+++ includes/structure.h
@@ -6,7 +6,7 @@
 /*   By: rozhou <rozhou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 14:39:24 by elabasqu          #+#    #+#             */
-/*   Updated: 2022/02/28 14:22:24 by rozhou           ###   ########.fr       */
+/*   Updated: 2022/03/02 12:19:39 by rozhou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ typedef struct s_data{
 	char	**env_path;
 	t_list	pwdpos;
 	t_list	oldpwdpos;
+	t_list	*lastenv;
 }				t_data;
 
 typedef struct s_bob {
